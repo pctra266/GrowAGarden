ffe5547147ec32c21668e537ef06a2fa7079814d
Tra Pham <146045106+pctra266@users.noreply.github.com>
2025-11-03 20:53:07 +0700

    Feat/rain event (#10)
    * feat: add event manager system and rainEffect
    
    * feat: growx2 when raining

--- Assets/Scripts/CropsManager.cs
+++ Assets/Scripts/CropsManager.cs
@@ -80,8 +80,12 @@ public class CropsManager : MonoBehaviour
     void Grow(Crop crop)
     {
         if (!crop.timerIsRunning) return;
-
-        crop.timeRemaining -= Time.deltaTime;
+        float delta = Time.deltaTime;
+        if (EventManager.instance != null && EventManager.instance.IsEventRunning("Rain"))
+        {
+            delta *= 2f; 
+        }
+        crop.timeRemaining -= delta;
         if (crop.timeRemaining > 0) return;
 
         crop.NextState();
